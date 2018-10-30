de8eaa7cd1b6bb2012fd292296408b59cdcf3921
Smile <236842785@qq.com>
2018-10-31 00:24:54 +0800

    1. 修改BUFF缓存大小

--- src/hardware/usart3.h
+++ src/hardware/usart3.h
@@ -3,8 +3,8 @@
 #include "sys.h"  
  
 
-#define USART3_MAX_RECV_LEN		1000					//最大接收缓存字节数
-#define USART3_MAX_SEND_LEN		600					//最大发送缓存字节数
+#define USART3_MAX_RECV_LEN		1024					//最大接收缓存字节数
+#define USART3_MAX_SEND_LEN		1024					//最大发送缓存字节数
 #define USART3_RX_EN 			1					//0,不接收;1,接收.
 
 extern u8  USART3_RX_BUF[USART3_MAX_RECV_LEN]; 		//接收缓冲,最大USART3_MAX_RECV_LEN字节
